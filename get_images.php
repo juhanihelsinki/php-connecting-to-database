0eec6115a2f4d7218c1e5e7f5313fbf5ecdb03b0
juhanihelsinki <44511950+juhanihelsinki@users.noreply.github.com>
2019-02-06 19:31:06 +0200

    Update get_images.php

--- get_images.php
+++ get_images.php
@@ -2,14 +2,14 @@ $dbhost='localhost:3306';
 $dbuser='xxxx_xxxx';
 $dbpass='yyyy_yyyy';
 $dbname='db_kuvat';
-$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-if(!$conn){
-die('Couldnotconnect:'.mysql_error());
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn){
+die('Could not connect: ' .mysql_error());
 }
 	$conn->set_charset('utf8');
 
 //construct SQL query
-if ($_GET[haku]!="") $sql = "SELECT * FROM taulukko WHERE kuvanumero LIKE '%$_GET[haku]%'";
+if ($_GET[haku] != "") $sql = "SELECT * FROM taulukko WHERE kuvanumero LIKE '%$_GET[haku]%'";
 $result = mysqli_query($conn, $sql) or die("Error retrieving data");
 
 //THE MAIN IMAGE PAGE IS CREATED HERE
@@ -26,11 +26,11 @@ mysqli_free_result($result);
 $new_img_arr = $_SESSION['img_arr'];
 $new_search_arr = $_SESSION['search_arr'];
 
-$avain = array_search((int)$kuvanumero,$new_img_arr);	
+$avain = array_search((int)$kuvanumero, $new_img_arr);	
 $next = $new_img_arr[$avain+1];
 $prev = $new_img_arr[$avain-1];
 			
-echo "<ahref='/kuvat/luettelo4.php?haku = $kuvanumero'><imgborder='0'src='/images/$vuosikymmen/images/$tiedostonimi'width='450'></a>";
+echo "<a href='/kuvat/luettelo4.php?haku = $kuvanumero'><imgborder='0'src='/images/$vuosikymmen/images/$tiedostonimi'width='450'></a>";
 echo "<h3>$otsikko</h3>";
 echo "<p>$kuvailu</p>";
 echo "<p>&nbsp;</p>";
@@ -38,13 +38,13 @@ echo "<p>&nbsp;</p>";
 if (!$new_search_arr[vuosikymmen]) echo "<p><i>Aikakausi: <a href='/kuvat/luettelo.php?vuosikymmen=$vuosikymmen'> $vuosikymmen-luku</a></p><p>&nbsp;</p></i>";	
 	
 if ($new_search_arr[henkilo]) echo "<p><a href='/kuvat/luettelo.php?henkilo=$new_search_arr[henkilo]'>Takaisinhakutuloksiin:henkilo=$new_search_arr[henkilo]</a>";
-elseif ($new_search_arr[teksti])echo"<p><a href='/kuvat/luettelo.php?teksti=$new_search_arr[teksti]'>Takaisinhakutuloksiin:teksti=$new_search_arr[teksti]</a>";
-elseif ($new_search_arr[vuosikymmen])echo"<p><a href='/kuvat/luettelo.php?vuosikymmen=$new_search_arr[vuosikymmen]'>Takaisinhakutuloksiin:vuosikymmen=$new_search_arr[vuosikymmen]</a>";
-else echo "<p><a href='index.php'>Kuvienhaku</a></p>";
-echo "<br><br><br><strong>Kuvankäyttöoikeudet:</strong>Creativecommons<span><atarget='_blank'href='http://creativecommons.org/licenses/by/4.0/deed.fi'>CCBY4.0</a></span>";
-if ($prev) echo "<pclass='irrallaan'><tablewidth='450'><tr><td><imgheight='0'width='85'src='/pics/250px.gif'></td><td><ahref='/kuvat/luettelo.php?haku=$prev'><imgwidth='30'src='/pics/success-previous-button.png'></a></td>";
+elseif ($new_search_arr[teksti]) echo"<p><a href='/kuvat/luettelo.php?teksti = $new_search_arr[teksti]'>Takaisinhakutuloksiin:teksti=$new_search_arr[teksti]</a>";
+elseif ($new_search_arr[vuosikymmen]) echo"<p><a href='/kuvat/luettelo.php?vuosikymmen=$new_search_arr[vuosikymmen]'>Takaisinhakutuloksiin:vuosikymmen=$new_search_arr[vuosikymmen]</a>";
+else echo "<p><a href='index.php'>Kuvien haku</a></p>";
+echo "<br><br><br><strong>Kuvan käyttöoikeudet: </strong> Creative commons<span><a target='_blank'href='http://creativecommons.org/licenses/by/4.0/deed.fi'>CCBY4.0</a></span>";
+if ($prev) echo "<pclass='irrallaan'><table width='450'><tr><td><img height='0' width='85' src='/pics/250px.gif'></td><td><a href='/kuvat/luettelo.php?haku=$prev'><img width='30'src='/pics/success-previous-button.png'></a></td>";
 else echo "<pclass='text_line'>&nbsp;";
-if($next) echo"<td><ahref='/kuvat/luettelo.php?haku=$next'><imgwidth='30'alt='button'src='/pics/success-next-button.png'></a></td><td><imgheight='0'width='75'src='/pics/250px.gif'></td></tr></table></p>";
+if ($next) echo "<td><ahref='/kuvat/luettelo.php?haku=$next'><imgwidth='30'alt='button'src='/pics/success-next-button.png'></a></td><td><imgheight='0'width='75'src='/pics/250px.gif'></td></tr></table></p>";
 echo "<p><p><hr></p></p>";
-echo "<p><p><pclass='text_line'><ahref='/kuvat/luettelo4.php?haku = $kuvanumero'>(+)suurennakuva[$kuvanumero]</a></p></p>";
+echo "<p><p><pclass='text_line'><ahref='/kuvat/luettelo4.php?haku=$kuvanumero'>(+)suurenna kuva [$kuvanumero]</a></p></p>";
 	
